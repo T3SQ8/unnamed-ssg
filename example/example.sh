8fb8e9654193f51f7dcb8319f59e2131aaf1a7b5
T3SQ8 <h9s@tutanota.com>
2021-05-30 17:59:54 +0200

    Added example files

---
+++ example/example.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. ssg-build
+
+genrss index.rec \
+	'Unnamed blog' \
+	'https://www.example.com' \
+	'My new unnamed personal blog' \
+	> rss.xml
+
+genindex index.rec > index.html
+
+dirindex archive > archiveindex.html
+
+wraphtml in_html.html > out_html.html
+
+pandoc -f gfm -f html in_markdown.md | wraphtml > out_markdown.html
